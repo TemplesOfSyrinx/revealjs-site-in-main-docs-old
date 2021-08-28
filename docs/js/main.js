d00dae84a94c187a62d4b60b8446b288b8ccd745
John Chambers-Malewig <johncm@users.noreply.github.com>
2021-08-27 21:58:20 -0500

    Add Plugins (#10)
    * Update ReadMe and add .gitignore for replit (#1)
    
    * 5 revealjs submodule (#2)
    
    * Add reveal.js as submodule
    
    * Add minimal html for slides
    
    * update locations, add reset.css (#3)
    
    * Add plugin init and examples (#4)

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,12 @@
-Reveal.initialize();
+// Also available as an ES module, see:
+// https://revealjs.com/initialization/
+Reveal.initialize({
+    // Learn about plugins: https://revealjs.com/plugins/
+    plugins: [
+        RevealZoom,
+        RevealNotes,
+        RevealSearch,
+        RevealMarkdown,
+        RevealHighlight
+    ]
+});
