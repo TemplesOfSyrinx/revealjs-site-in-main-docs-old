cb67a5f3e08fcf1188c309dfab621aa750ea4d5d
John Chambers-Malewig <johncm@users.noreply.github.com>
2021-08-27 22:41:08 -0500

    Add reveal.js-menu (#12)
    * Update ReadMe and add .gitignore for replit (#1)
    
    * 5 revealjs submodule (#2)
    
    * Add reveal.js as submodule
    
    * Add minimal html for slides
    
    * update locations, add reset.css (#3)
    
    * Add plugin init and examples (#4)
    
    * Add reveal.js-menu and init (#5)

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,26 @@
 // Also available as an ES module, see:
 // https://revealjs.com/initialization/
 Reveal.initialize({
+    menu: {
+        themes: true,
+        // Specifies the path to the default theme files.
+        themesPath: 'revealjs/dist/theme/',
+        custom: [
+            {
+                title: 'About',
+                icon: '<i class="fa fa-info">',
+                content: '<p>This slidedeck is created with reveal.js</p>'
+                // OR src: 'links.html'
+            }
+        ]
+    },
     // Learn about plugins: https://revealjs.com/plugins/
     plugins: [
         RevealZoom,
         RevealNotes,
         RevealSearch,
         RevealMarkdown,
-        RevealHighlight
+        RevealHighlight,
+        RevealMenu
     ]
 });
